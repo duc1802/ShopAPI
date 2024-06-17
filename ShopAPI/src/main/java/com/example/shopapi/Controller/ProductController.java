a72d2f8dbf87890951f5e5369d9f7ffc72283587
phuch <phuchmth2209055@fpt.edu.vn>
2024-06-17 09:53:29 +0700

    1 commit

--- ShopAPI/src/main/java/com/example/shopapi/Controller/ProductController.java
+++ ShopAPI/src/main/java/com/example/shopapi/Controller/ProductController.java
@@ -4,12 +4,16 @@ import com.example.shopapi.Entity.Product;
 import com.example.shopapi.Repository.CustomerRepository;
 import com.example.shopapi.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ProductController {
@@ -21,10 +25,19 @@ public class ProductController {
         this.customerRepository = customerRepository;
     }
 
-    @GetMapping("/")
-    public String productList(Model model) {
-        List<Product> products = productRepository.findAll();
-        model.addAttribute("products",products);
-        return "product-list";
+    @GetMapping
+    public ResponseEntity<Page<Product>> getAll(Pageable pageable) {
+        return ResponseEntity.ok(productRepository.findAll(pageable));
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getById(@PathVariable Integer id) {
+        Optional<Product> optionalProduct = productRepository.findById(id.longValue());
+        if (!optionalProduct.isPresent()) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
+        return ResponseEntity.ok(optionalProduct.get());
+    }
+
 }
